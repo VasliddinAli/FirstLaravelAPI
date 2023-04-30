e66aadb7a74cfc8fe18b2ffc6c4a91fc9b9ff2fb
VasliddinAli <vasliddinali@gmail.com>
2023-04-30 17:26:05 +0500

    add Image

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,16 +11,23 @@ class PostController extends Controller
     public function index()
     {
         return response()->json([
-            'posts' => Post::get()->each(function($item){
+            'posts' => Post::get()->each(function ($item) {
                 $post = $item;
                 $post['date'] = Carbon::parse($post->created_at)->format('H:i:s');
-                unset($post->created_at, $post->updated_at);
             })
         ]);
     }
     public function store(Request $request)
     {
+        $file = $request->file('file');
+        $imageName = time() . '.' . $file->extension();
+        $imagePath = public_path() . '/images';
+        $image = 'images/' . $imageName;
+
+        $file->move($imagePath, $imageName);
+
         $post = new Post;
+        $post->image = $image;
         $post->title = $request->title;
         $post->description = $request->description;
         $post->save();
@@ -36,18 +43,39 @@ class PostController extends Controller
     }
     public function update(Request $request, Post $post)
     {
-        $post->title = $request->title;
-        $post->description = $request->description;
-        $post->save();
-        return response()->json([
-            'message' => 'Post Updated',
-            'status' => 'success',
-            'data' => $post
-        ]);
+        if ($request->file('file')) {
+            unlink($post->image);
+            $file = $request->file('file');
+            $imageName = time() . '.' . $file->extension();
+            $imagePath = public_path() . '/images';
+            $image = 'images/' . $imageName;
+
+            $file->move($imagePath, $imageName);
+
+            $post->image = $image;
+            $post->title = $request->title;
+            $post->description = $request->description;
+            $post->save();
+            return response()->json([
+                'message' => 'Post Created',
+                'status' => 'success',
+                'data' => $post
+            ]);
+        } else {
+            $post->title = $request->title;
+            $post->description = $request->description;
+            $post->save();
+            return response()->json([
+                'message' => 'Post Updated',
+                'status' => 'success',
+                'data' => $post
+            ]);
+        }
     }
     public function destroy(Post $post)
     {
         $post->delete();
+        unlink($post->image);
         return response()->json([
             'message' => 'Post deleted',
             'status' => 'success'
--- routes/api.php
+++ routes/api.php
@@ -22,5 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('posts', [PostController::class, 'index']);
 Route::post('posts', [PostController::class, 'store']);
 Route::get('posts/{post}', [PostController::class, 'show']);
-Route::put('posts/{post}', [PostController::class, 'update']);
+Route::post('posts/{post}', [PostController::class, 'update']);
 Route::delete('posts/{post}', [PostController::class, 'destroy']);
